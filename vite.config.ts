b409afeb22a5e6752309aa98eee31acff39f68a5
Adriana Sobota <universe7raindrop@gmail.com>
2024-03-24 17:45:26 +0100

    cors fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/api': 'https://5d55bd72-55b9-4785-8f11-e06014ee8714-dev.e1-us-cdp-2.choreoapis.dev/rmmd/spotifymoodbackend/now-popular-service-e9d/v1.0' 
+    }
+  }
 })
