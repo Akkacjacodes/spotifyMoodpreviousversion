449c2fcf5361736994ae91c548113f8d6a513f74
Adriana Sobota <universe7raindrop@gmail.com>
2024-04-29 23:11:56 +0200

    authentication header

--- src/pages/NewPopular.tsx
+++ src/pages/NewPopular.tsx
@@ -15,9 +15,11 @@ type DataState = {
   message: string;
   playlists: PlaylistItem[];
 };
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+const baseURL = 'https://5d55bd72-55b9-4785-8f11-e06014ee8714-prod.e1-us-cdp-2.choreoapis.dev/rmmd/spotifymoodbackend/now-popular-service-e9d/v1.0'
+// const baseURL = import.meta.env.VITE_API_BASE_URL;
 console.log(baseURL);
-const yourAccessToken= import.meta.env.yourAccessToken;
+// const yourAccessToken= import.meta.env.yourAccessToken;
 
 const NewPopular = () => {
   const [data, setData] = useState<DataState>({ message: "", playlists: [] });
@@ -28,7 +30,7 @@ const NewPopular = () => {
     try {
       const config = {
         headers: {
-          "Authorization": `Bearer ${yourAccessToken}`, // Replace 'your_access_token' with the actual token
+          "Authorization": `Bearer eyJraWQiOiJnYXRld2F5X2NlcnRpZmljYXRlX2FsaWFzIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJlMzg3MjBjOC1lN2IxLTQ1MWMtYjI1Zi0wNDY5YWMyYmI3ZjZAY2FyYm9uLnN1cGVyIiwiYXVkIjoiY2hvcmVvOmRlcGxveW1lbnQ6cHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOlwvXC9zdHMuY2hvcmVvLmRldjo0NDNcL2FwaVwvYW1cL3B1Ymxpc2hlclwvdjJcL2FwaXNcL2ludGVybmFsLWtleSIsImtleXR5cGUiOiJQUk9EVUNUSU9OIiwic3Vic2NyaWJlZEFQSXMiOlt7InN1YnNjcmliZXJUZW5hbnREb21haW4iOm51bGwsIm5hbWUiOiJzcG90aWZ5IHByZXZpb3VzIHZlcnNpb24gYmFja2VuZCAtIE5ldyBQb3B1bGFyIFNlciIsImNvbnRleHQiOiJcLzVkNTViZDcyLTU1YjktNDc4NS04ZjExLWUwNjAxNGVlODcxNFwvcm1tZFwvc3BvdGlmeS1wcmV2aW91cy12ZXJzaW9uLWJhY2tlbmRcL25ldy1wb3B1bGFyLXNlcnZpY2UtNDQwXC92MS4wIiwicHVibGlzaGVyIjoiY2hvcmVvX3Byb2RfYXBpbV9hZG1pbiIsInZlcnNpb24iOiJ2MS4wIiwic3Vic2NyaXB0aW9uVGllciI6bnVsbH1dLCJleHAiOjE3MTQ0MjU2MTIsInRva2VuX3R5cGUiOiJJbnRlcm5hbEtleSIsImlhdCI6MTcxNDQyNTAxMiwianRpIjoiMDczNjQwY2EtMWRkYS00ZmY5LTljYzItNTM3YzEwMTVlOGM1In0.RksW5pEOJp7thK96l_sJP0S1ZA3OxwR2tfadySmql2v18FdfooSoqF6qRBlOJQ9Au2i08u-3we_ACUkZc16LWfw1MmR4YNagPm05AJqtsADsBBf3O5UU1YI6fVFX_jIyCl-oW1DVEoYjyALn82n1RLxHOElFm-i94Mit4az88LPUX8LJ5N2DrI9QeUi5HByuZYMxGRgtEvuUJF6Chnn-ThDCvgIQocbqCHSsg0dO3yCmodzwtAAnCm5FaQiM-ilSaWNYZBvKUr2OYzUEg9U2nYARog6JcuygDuCZDDdaPQG5whs9LE0G5NpxA1b1zZl1GLxWP4Vu82em7d0tS3gmO1TwLIYg_SZZ-Ce-Hnyc1kSsK_le93Ia1q7joDwrAOUdgItFL_eH9gdnRAkCqg1vldab3Q9LKmE0kMfdk2t9ioKMoCiml_KTOZ2MCbzuskKtKWRRWZaklelpwYRYqQraTpH96ObKWxS5dCCnumVmTLnjFryfU0GfYO0_wL9uZBT8T5XjGh_3VqEjcL98A1NMtjQixrUTj4AeRTZKspmIEB6Lc0KhNyengKfk8uAHM6p4_Z5THC0SJgbiY8nRyKKNTHa4PZhMbFWLSH4HWaoa4RDg46iNfJm8Cu1H6kFiqlMhjT5w_YOkOQpc_WmcMAp883IXpg0Vl7LLu3xXBf4Hjco`, // Replace 'your_access_token' with the actual token
           "Custom-Header": "Hello", // Add any other headers here
         },
       };
